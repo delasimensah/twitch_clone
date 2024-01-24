53c9579e1b124cb462caaca49bc55d432bb6e298
delasimensah <mensadelasi@gmail.com>
2024-01-24 10:28:22 +0000

    added keys settings page

---
+++ app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { CheckCheck, Copy } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+type CopyButtonProps = {
+  value?: string;
+};
+
+export const CopyButton = ({ value }: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    if (!value) return;
+
+    setIsCopied(true);
+    navigator.clipboard.writeText(value);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const Icon = isCopied ? CheckCheck : Copy;
+
+  return (
+    <Button
+      onClick={handleCopy}
+      disabled={!value || isCopied}
+      variant="ghost"
+      size="sm"
+    >
+      <Icon className="h-4 w-4" />
+    </Button>
+  );
+};
